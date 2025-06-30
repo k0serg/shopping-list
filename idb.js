a294206c4ff1afd47046b15700f60f66b3aec1ab
k0serg <k0serg@internet.ru>
2025-06-30 22:13:23 +0300

    Delete idb.js

--- idb.js
+++
@@ -1,8 +0,0 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.idb = {}));
-})(this, (function (exports) {
-  // Полная реализация idb.js
-  // Для упрощения можно использовать CDN: https://cdn.jsdelivr.net/npm/idb @7/build/umd.min.js
-}));
